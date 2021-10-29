54293c5a667d57c326cf48b8034006b36db764e8
ekkys <ekkys99@gmail.com>
2021-10-29 10:48:02 +0700

    [+] Add some layout : email confirm & activated

---
+++ resources/views/email-success.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Password Reset - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card border-0 rounded-lg mt-5">
+                                    <div class="d-flex justify-content-center">
+                                        <h3 class="text-center font-weight-light my-4"></h3>
+                                         <img  src="assets/img/logo_ps_long - Copy.png" alt="logo_ps_long" style="height:100px; width:auto;">
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="small mb-3 text-muted d-flex justify-content-center"></div>
+                                        <div class="card mb-4">
+                                            <div class="card-body">
+                                                <p class="mb-0">
+                                                   Aktivasi Akun anda telah berhasil!
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                   <div class="text-center py-3">
+                                        <a class="btn btn-lg btn-info text-white px-5 py-1 rounded-xl" href="login.html"><i class="fas fa-sign-in-alt"></i> Back to Login Screen</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
