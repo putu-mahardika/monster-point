52bf342727d5bf2c6570db8527d51b6f43a13aef
ekkys <ekkys99@gmail.com>
2021-10-29 09:31:51 +0700

    [+] Add register  page

---
+++ resources/views/register.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Login - SB Admin</title>
+  <link href="css/styles.css" rel="stylesheet" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous">
+  </script>
+  <style>
+
+  </style>
+</head>
+
+<body class="bg-white">
+  <div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+      <main>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-5">
+                  <div class="row justify-content-center mt-5 mb-5">
+                    <div class="col-auto">
+                       <img  src="assets/img/logo_ps_long - Copy.png" alt="logo_ps_long" style="height:100px; width:auto;">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <form>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="Nama Perusahaan" aria-label="Nama Perusahaan" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="Alamat Perusahaan" aria-label="Alamat Perusahaan" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="Person in Charge (PIC)" aria-label="Person in Charge (PIC)" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="PIC Phone" aria-label="PIC Phone" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control rounded-xxl" placeholder="Kebutuhan" aria-label="Kebutuhan" aria-describedby="basic-addon1">
+                            </div>
+                      </form>
+
+                        <div class="d-flex justify-content-end">
+                          <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                          <a class="small" href="#"> Saya Setuju Syarat & Ketentuan </a>
+                        </div>
+                        <div class="d-flex justify-content-center mt-5">
+                          <a class="btn btn-lg btn-info text-white px-5 py-1 rounded-xl" href="index.html">Register</a>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                <div class=" text-center py-3">
+                  <div class="small">
+                    <a href="login.html">Sudah punya akun? Masuk ke halaman Login</a>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <div id="layoutAuthentication_footer">
+      <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+          <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Point Service 2021</div>
+            <div>
+              <a href="#">Privacy Policy</a>
+              &middot;
+              <a href="#">Terms &amp; Conditions</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+  </script>
+  <script src="js/scripts.js"></script>
+</body>
+
+</html>
