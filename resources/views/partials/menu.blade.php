a36993d938c9f066c0683d22061b99929893f866
ekkys <ekkys99@gmail.com>
2021-11-01 12:17:48 +0700

    Update side bar menu fix look as the design

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,35 @@
+
+ <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion" style="background-color: #E5E5E5;">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <a class="nav-link mb-3 mt-3 menu-halfround" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-building"></i></div>
+                                Company
+                            </a>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-store"></i></div>
+                                Merchant
+                            </a>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fa fa-calculator" aria-hidden="true"></i></div>
+                               Event / Formula
+                            </a>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-receipt"></i></div>
+                                Billing
+                            </a>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="far fa-question-circle"></i></div>
+                                Help
+                            </a>
+
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
