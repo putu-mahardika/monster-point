bf56956f7a214a96feefc1f55593c39cd19014e3
ekkys <ekkys99@gmail.com>
2021-11-04 10:52:38 +0700

    Help belum selesai

--- resources/views/pages/help/index.blade.php
+++ resources/views/pages/help/index.blade.php
@@ -18,7 +18,18 @@
             <div class="card-body">
                  <div class="row">
                     <div class="col-auto">
-
+                        <div class="row">
+                            <div class="span6">
+                                <div class="mycontent-left">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, molestias saepe magni maiores excepturi quia voluptatum placeat ipsam sed dolorem, iusto dolor, nihil adipisci officiis dolore hic fuga dolores ex!
+                                </div>
+                            </div>
+                            <div class="span6">
+                                <div class="mycontent-right">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </div>
+                            </div>
+                            </div>
                     </div>
                  </div>
             </div>
