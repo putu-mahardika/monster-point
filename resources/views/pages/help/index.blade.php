4effe0353b08b43b1ba309b7b9f831f6fe6795ce
ekkys <ekkys99@gmail.com>
2021-11-04 10:37:12 +0700

    [+] Add Help page

--- resources/views/pages/help/index.blade.php
+++ resources/views/pages/help/index.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.main')
+
+@section('meta')
+
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('title', 'Help')
+
 @section('content')
 
-<h1>ini help page</h1>
+<div class="row">
+    <div class="col">
+        <div class="card rounded-xxl">
+            <div class="card-body">
+                 <div class="row">
+                    <div class="col-auto">
 
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('modal')
+
+@endsection
+
+@section('js')
 @endsection
