d5531aa495aef1fb2c3371cd17842e144eedf55e
ekkys <ekkys99@gmail.com>
2021-11-08 11:21:16 +0700

    Update help index view

--- resources/views/pages/help/index.blade.php
+++ resources/views/pages/help/index.blade.php
@@ -1,8 +1,4 @@
 @extends('layouts.main')
-<<<<<<< HEAD
-
-=======
->>>>>>> a1706f2bdc5c1cc327c0709a6de0193fd9d4ade4
 @section('meta')
 
 @endsection
@@ -11,124 +7,72 @@
 
 @endsection
 
-@section('title', 'Help')
+@section('title', 'Billing')
 
 @section('content')
-<div class="col-md-12">
-    <div class="card mb-4 rounded-xxl">
-        <div class="card-body ">
-            <div class="row mt-3  ">
-                <div class="col">
-                  <nav id="navbar-example3" class="navbar navbar-light bg-light flex-column align-items-stretch p-3">
-                    <a class="navbar-brand" href="#">Navbar</a>
+<div class="row">
+    <div class="col-auto">
+        <div class="card mb-4 rounded-xxl">
+            <div class="card-body">
+                <div class="row mt-2 ">
+                    <nav id="navbar-example3" class="navbar navbar-light flex-column align-items-stretch p-3">
+                        <a class="navbar-brand" href="#">Navbar</a>
                         <nav class="nav nav-pills flex-column">
                             <a class="nav-link" href="#item-1">Item 1</a>
                                 <nav class="nav nav-pills flex-column">
                                     <a class="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
                                     <a class="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
                                 </nav>
-                                    <a class="nav-link" href="#item-2">Item 2</a>
-                                    <a class="nav-link" href="#item-3">Item 3</a>
+                            <a class="nav-link" href="#item-2">Item 2</a>
+                            <a class="nav-link" href="#item-3">Item 3</a>
                                 <nav class="nav nav-pills flex-column">
                                     <a class="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
                                     <a class="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
                                 </nav>
                         </nav>
                     </nav>
-
-                </div>
-                <div class="col">
-                   <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0">
-                        <h4 id="item-1">Item 1</h4>
-                            <p>
-                                This is some placeholder content for the scrollspy page.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                This is some placeholder content for the scrollspy page.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                This is some placeholder content for the scrollspy page.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h5 id="item-1-1">Item 1-1</h5>
-                            <p> Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h5 id="item-1-2">Item 1-2</h5>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h4 id="item-2">Item 2</h4>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h4 id="item-3">Item 3</h4>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-
-                            </p>
-                            <h5 id="item-3-1">Item 3-1</h5>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h5 id="item-3-2">Item 3-2</h5>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                            <h5 id="item-1-1">Item 1-1</h5>
-                            <p>
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                                Note that as you scroll down the page, the appropriate navigation link is highlighted.
-                                 It's repeated throughout the component example. We keep adding
-                                some more example copy here to emphasize the scrolling and highlighting.
-                            </p>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="col-md-10">
+        <div class="card mb-4 rounded-xxl">
+            <div class="card-body">
+                <div class="row mt-2">
+                    <div class="col p-3">
+                        <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0">
+                            <h4 id="item-1">Item 1</h4>
+                            <p>...</p>
+                            <h5 id="item-1-1">Item 1-1</h5>
+                            <p>...</p>
+                            <h5 id="item-1-2">Item 1-2</h5>
+                            <p>...</p>
+                            <h4 id="item-2">Item 2</h4>
+                            <p>...</p>
+                            <h4 id="item-3">Item 3</h4>
+                            <p>...</p>
+                            <h5 id="item-3-1">Item 3-1</h5>
+                            <p>...</p>
+                            <h5 id="item-3-2">Item 3-2</h5>
+                            <p>...</p>
+                        </div>
+                    </div>
+                    <div class="vr"></div>
+                    <div class="col p-3">
+                        <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0">
+                            <h4 id="item-1">Item 1</h4>
+                            <p>Code Mirror here</p>
 
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
 @section('js')
 @endsection
