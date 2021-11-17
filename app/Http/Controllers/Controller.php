69ead30692c4e3d763413e3ddb4e2965b19976ea
Putu Mahardika <mahardikaputu13@gmail.com>
2021-11-17 10:44:20 +0700

    Revert "Merge remote-tracking branch 'origin/landing-page' into putu"
    This reverts commit ca5b854dbdf1eb790225eb67b4b189d646e9679f, reversing
    changes made to a28ffd18a3de45128198084b0037413bd102d8b2.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,12 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-/**
- * @OA\Info(
- *    title="Dokumentasi Monster Point Service",
- *    version="1.0.0",
- * )
- */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
