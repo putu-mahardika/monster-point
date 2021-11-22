0b895349705d8d953d3941966a9f255a4e39dffd
Putu Mahardika <mahardikaputu13@gmail.com>
2021-11-22 15:14:17 +0700

    [+] create model & controller Billing, BillingDetail

---
+++ app/Models/Billing.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Billing extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Billing';
+    protected $primaryKey = 'Id';
+
+    const CREATED_AT = 'CreateDate';
+    const UPDATED_AT = 'LastUpdate';
+
+    protected $guarded = ['Id', 'Guid', 'CreateDate', 'LastUpdate'];
+    protected $hidden = ['Times'];
+
+    public function billing_detail()
+    {
+        return $this->hasMany(BillingDetail::class, 'billing_id', 'Id');
+    }
+}
---
+++ app/Models/BillingDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BillingDetail extends Model
+{
+    use HasFactory;
+
+    public $table = 'billing_detail';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function billing()
+    {
+        return $this->belongsTo(BillingDetail::class, 'billing_id', 'Id');
+    }
+
+}
