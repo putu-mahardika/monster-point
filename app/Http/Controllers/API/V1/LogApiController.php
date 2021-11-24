453fd3ff8ed1d4cdfd15d3f0d78f722caf311d6b
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-11-24 10:18:46 +0700

    Fix api structure

--- app/Http/Controllers/API/V1/LogApiController.php
+++ app/Http/Controllers/API/V1/LogApiController.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\API\v1;
+namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helpers\LogHelper;
-use DB;
 
 class LogApiController extends Controller
 {
     // public function index($token, $event, $id, $value){
     //     return LogHelper::indexLogApi($token, $event, $id, $value);
     // }
-    public function index(Request $request, $token, $event, $id, $value){
+    public function transaction(Request $request, $token, $event, $id, $value){
         return LogHelper::indexLogApi($request, $token, $event, $id, $value);
 
     }
