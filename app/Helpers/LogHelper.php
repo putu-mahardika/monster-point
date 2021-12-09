0f2eb7c4a58dd33d70fe6b4c5ce2dae768175cf0
Putu Mahardika <mahardikaputu13@gmail.com>
2021-12-09 13:57:08 +0700

    [=] update log error event

--- app/Helpers/LogHelper.php
+++ app/Helpers/LogHelper.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Helpers;
 
+use App\Models\Event;
 use App\Models\Log;
+use App\Models\Member;
+use App\Models\Merchant;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
 
 class LogHelper {
     public static function indexLogApi(Request $request, $token, $event, $id, $value){
@@ -17,12 +21,31 @@ class LogHelper {
             ]);
             unset($exec[0]->Times);
         } catch (\Exception $e) {
+            $idMerchant = Merchant::where('Token', $token)->pluck('Id')->first();
+            $idEvent = Event::where('Kode', $event)->where('IdMerchant', $idMerchant)->pluck('Id')->first();
+            $idMember = Member::where('MerchentMemberKey', $id)->where('IdMerhant', $idMerchant)->pluck('Id')->first();
+            $guid = Str::upper(Str::uuid());
+
+            Log::create([
+                'Guid' => $guid,
+                'IdMerchant' => $idMerchant,
+                'IdMember' => $idMember,
+                'IdEvent' => $idEvent,
+                'Node' => $event,
+                'Keterangan' => $e->getMessage(),
+                'Point' => 0,
+                'Status' => 400,
+            ]);
             return response()->json(['message' => 'Something went wrong!'], 400);
         }
         return response()->json($exec);
     }
 
     public static function memberHistoryPoint(Request $request, $token, $id){
+
+        $idMerchant = Merchant::where('Token', $token)->pluck('Id')->first();
+        $idMember = Member::where('MerchentMemberKey', $id)->where('IdMerhant', $idMerchant)->pluck('Id')->first();
+        $exec = Log::select('CreateDate', 'Point')->where('IdMerchant', $idMerchant)->where('IdMember', $idMember)->get();
 
         try {
             $exec = DB::table('Log')
