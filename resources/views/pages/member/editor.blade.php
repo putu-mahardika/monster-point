72cffe5275594692200955ebba0a8f66ebe8f2c0
Putu Mahardika <mahardikaputu13@gmail.com>
2022-01-07 13:44:20 +0700

    [=] fixed kondisi show merchant select

--- resources/views/pages/member/editor.blade.php
+++ resources/views/pages/member/editor.blade.php
@@ -17,9 +17,31 @@
         @method('PATCH')
     @endif
             @csrf
-            @if (!auth()->user()->is_admin)
-                <input type="hidden" name="merchant_id" value="{{ auth()->user()->merchant->Id }}">
+            @if (request()->route()->getName() == 'members.create')         <!-- Jika tampil form create -->
+                @if (!auth()->user()->is_admin)         <!-- Jika dibuka oleh selain admin (merchant) -->
+                    <input type="hidden" name="merchant_id" value="{{ auth()->user()->merchant->Id }}">     <!-- get id dari logged merchant -->
+                @else       <!-- jika dibuka oleh admin -->
+                    @if ($request->m)   <!-- jika ada merchant_id dari request -->
+                        <input type="hidden" name="merchant_id" value="{{ $request->m }}">
+                    @else       <!-- jika request ga punya merchant_id -->
+                        <div class="row justify-content-center mb-3">
+                            <div class="col-md-5">
+                                <label for="merchant_id">Merchant</label>
+                            </div>
+                            <div class="col-md-7">
+                                <select class="form-select rounded-xl" id="merchant_id" name="merchant_id" >
+                                    <option></option>
+                                    @foreach ($merchants as $merchant)      <!-- $merchants dapet dari MemberController@create -->
+                                        <option value="{{ $merchant->Id }}">{{ $merchant->Nama }}</option>
+                                    @endforeach
+                                </select>
+                                <x-error-message-field for="merchant_id" class="d-none"></x-error-message-field> --}}
+                            </div>
+                        </div>
+                    @endif
+                @endif
             @endif
+
             <div class="row justify-content-center mb-3">
                 <div class="col-md-5">
                     <label for="member_key">Member Key</label>
@@ -53,6 +75,13 @@
         </form>
 </div>
 <script>
+    $(document).ready(function() {
+        $('#merchant_id').select2({
+            dropdownParent: $('#addMemberModal'),
+            theme: "bootstrap-5",
+            placeholder: "Select a merchant",
+        });
+    });
     $(function () {
         $('#memberForm').on('submit', function(e) {
             e.preventDefault();
