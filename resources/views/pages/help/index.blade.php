10abd6a696f7e67a19a1c129e07eb61cd1c136a4
Putu Mahardika <mahardikaputu13@gmail.com>
2022-01-07 13:43:41 +0700

    [=] fixed height iframe swagger

--- resources/views/pages/help/index.blade.php
+++ resources/views/pages/help/index.blade.php
@@ -126,5 +126,5 @@
 
     {{-- </div> --}}
 
-<iframe src="http://127.0.0.1:8000/api/documentation" frameborder="0"  width = "101%" height = "600"></iframe>
+<iframe src="http://127.0.0.1:8000/api/documentation" frameborder="0" style="height: 100vh" width = "101%" height = "100vh"></iframe>
 @endsection
