74e3e006f0b3c88b0c01d64c9c6ef182fa0e7be1
Putu Mahardika <mahardikaputu13@gmail.com>
2022-01-07 13:42:54 +0700

    [=] fixed table member

--- resources/views/pages/member/index.blade.php
+++ resources/views/pages/member/index.blade.php
@@ -9,58 +9,52 @@
 
 @endsection
 @section('content')
-    <div class="card rounded-xxl">
-        <div class="card-body" style="min-height: calc(100vh - 10.3rem);">
-            {{-- <div class="row mb-3">
-                <div class="col">
-                    <div class="row">
-                        <div class="col-auto py-2">
-                            Key <i class="fas fa-info-circle" style="color: var(--ekky-cyan);"></i>
-                        </div>
-                        <div class="col">
-                            <div class="input-group mb-3">
-                                <input type="text" name="key" id="key" class="form-control rounded-xl-start border-end-0">
-                                <span class="btn rounded-xl-end border border-start-0" style="background-color: var(--ekky-light-gray);">
-                                    <i class="fas fa-pencil-alt"></i>
-                                </span>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card rounded-xxl auto-align">
+                <div class="card-body" style="min-height: calc(100vh - 10.3rem);">
+
+                    @can('members create')
+                        <div class="row mb-3">
+                            <div class="col-auto">
+                                <button type="button" id="create" class="btn btn-primary rounded-xxl" data-bs-toggle="modal" data-bs-target="#addMemberModal">
+                                    Add Member <i class="fas fa-plus ms-2"></i>
+                                </button>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col">
+                    @endcan
+
                     <div class="row">
-                        <div class="col-auto py-2">
-                            Starting Point
-                        </div>
                         <div class="col">
-                            <div class="input-group mb-3">
-                                <input type="text" name="starting_point" id="starting_point" class="form-control rounded-xl-start border-end-0">
-                                <span class="btn rounded-xl-end border border-start-0" style="background-color: var(--ekky-light-gray);" type="button">
-                                    <i class="fas fa-pencil-alt"></i>
-                                </span>
-                            </div>
+                            <div id="memberTable"></div>
                         </div>
                     </div>
                 </div>
-            </div> --}}
+            </div>
+        </div>
 
-            @can('members create')
-                <div class="row mb-3">
-                    <div class="col-auto">
-                        <button type="button" id="create" class="btn btn-primary rounded-xxl" data-bs-toggle="modal" data-bs-target="#addMemberModal">
-                            Add Member <i class="fas fa-plus ms-2"></i>
-                        </button>
+        <div class="col-md-4">
+            <div class="card rounded-xxl auto-align">
+                <div class="card-body position-relative" style="min-height: calc(100vh - 10.3rem);">
+                    <div class="row justify-content-between mb-3">
+                        <div class="col-auto fw-bold">
+                            <span id="record-name">Records</span>
+                        </div>
+                        <div class="col-auto">
+                            <span id="total-record">0</span> <i class="fas fa-user ms-1"></i>
+                        </div>
                     </div>
-                </div>
-            @endcan
 
-            <div class="row">
-                <div class="col">
-                    <div id="memberTable"></div>
+                    <div class="row mb-5">
+                        <div class="col">
+                            <div id="pointTable"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
 
 @section('modal')
@@ -114,11 +108,18 @@
                 $(this).find('#member_key').focus();
             });
 
+            getHistoryPoint();
+
+            // ==============================>>>> Get Data Member <<<<=============================
+
             eventTable = $('#memberTable').dxDataGrid({
                 dataSource: `{{ route('dx.members', auth()->user()->merchant->Id ?? null) }}`,
                 keyExpr: 'Id',
                 columnAutoWidth: true,
                 hoverStateEnabled: true,
+                selection: {
+                    mode: "single" // or "multiple" | "none"
+                },
                 columns: [
                     {
                         caption: '#',
@@ -168,7 +169,68 @@
                 showBorders: false,
                 showColumnLines: false,
                 showRowLines: true,
+                activeStateEnabled: true,
+                onSelectionChanged(selectedItems) {
+                    const data = selectedItems.selectedRowsData[0];
+                    if (data) {
+                        console.log(data.MerchentMemberKey);
+                        console.log(data.merchant.Token);
+                        let merchantMemberKey = data.MerchentMemberKey;
+                        let merchantToken = data.merchant.Token;
+                        getHistoryPoint(merchantToken, merchantMemberKey);
+                        getCountHistoryPoints(merchantToken, merchantMemberKey);
+                    }
+                },
             }).dxDataGrid('instance');
+
+            // ==============================>>>> Get Data Member <<<<=============================
+
+            // ==============================>>>> Get History Point Member <<<<=============================
+            function getHistoryPoint(token = '', id = ''){
+                memberTable = $('#pointTable').dxDataGrid({
+                    dataSource: `{{ url('members/getHistoryPoints') }}?token=${token}&id=${id}`,
+                    paging: {
+                        pageSize: 10
+                    },
+                    columnAutoWidth: true,
+                    hoverStateEnabled: true,
+                    columns: [
+                        {
+                            caption: '#',
+                            cellTemplate: function(container, options) {
+                                container.html(`${options.row.rowIndex + 1}`);
+                            }
+                        },
+                        {
+                            dataField: 'CreateDate',
+                            dataType: 'datetime',
+                            format: "dd-MM-yyyy HH:mm:ss"
+                        },
+                        {
+                            dataField: 'Point',
+                            dataType: 'number'
+                        },
+                    ],
+                    showBorders: false,
+                    showColumnLines: false,
+                    showRowLines: true,
+                }).dxDataGrid('instance');
+            }
+
+            // ==============================>>>> Get History Point Member <<<<=============================
+
+            // ====================================>>>> Get Count Data Member <<<<====================================
+
+            function getCountHistoryPoints(token = '', id = '') {
+                $.get(`{{ url('members/getCountHistoryPoints') }}?token=${token}&id=${id}`, function(data, status){
+                    // alert("Data: " + data + "\nStatus: " + status);
+                    document.getElementById('total-record').innerText = data;
+                });
+            }
+            // ====================================>>>> Get Count Data Member <<<<====================================
+
+
+
 
             // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Call modal Create Data >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
             $(document).on('click', '#create', function () {
